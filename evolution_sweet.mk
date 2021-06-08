0020ba7d69fa8253ae4ab634b8998991d4f810d4
Sushrut Gupta <sushrut.gupta@yahoo.com>
2021-06-08 15:38:21 +0530

    arrow --> evolution

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -6,9 +6,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/evolution/config/common.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -16,4 +16,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-DEVICE_MAINTAINER := daniml3
+DEVICE_MAINTAINER := Sushrut Gupta ( @Sushrut1101 )
